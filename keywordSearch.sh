7361002e5c21b947fe4bb49f703d04fea6f9d5ff
jkeddo_ <jjkeddo@gmail.com>
2023-03-19 14:45:39 -0700

    POC-C--Fix/Refactor-Keyword-Search
    Fixed keyword search to 1) Make it less specific about what data format it expects 2) To **not** strip off filepaths (tradeoff not worth the trouble+complexity) 3) To search nonwhisper log directories 4) To include a "clean" history section to keep track of things more easily in Git.

--- keywordSearch.sh
+++ keywordSearch.sh
@@ -2,10 +2,28 @@ echo "Please enter the keyword you would like to search..."
 read SEARCHKEY
 
 
-grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/'
+FILENAME="/Users/jibreelkeddo/DailyLogs/Searches/SearchResults"
+TIMESTAMP_YEARMONTH=`date "+%Y%m"`
+echo "Searching for $SEARCHKEY"
+echo "Old search mods below"
 
-### old search params/refinements
-#| sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' | less -R
+echo "<html><body style=\"background-color:grey;\">" > $FILENAME.html
+grep -i -r "$1" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/$/<br><br>/' >> $FILENAME.html
+echo "</body></html>" >> $FILENAME.html
+
+open $FILENAME.html
+
+
+
+##################################################
+##### old/broken/retired keywordSearchQueries#####
+##################################################
+# grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' | less -R
+# grep -i -r "$1" /Users/jibreelkeddo/DailyLogs/Whispers/* | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' >> $FILENAME.html
+# grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/'
+##################################################
+##################################################
+
 
 echo "\n\nKeyword search completed!"
 
