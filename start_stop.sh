1e39d84a0a07a7b616e5c4c12df3c633bbe3a24a
jkeddo_ <jjkeddo@gmail.com>
2023-03-19 14:13:55 -0700

    POC-C--Basically-Perfect-Optimized-StartStop-For-Processing-Time-Offset
    This commit is for saving what is basically a perfectly optimized start-stop script which has an average 3 word overlap offset between messages after accounting for extra processing time. This processing time is a definitive reference tuning for M1 Pro Mac Config + Large-V2 model + print-colors-and-textfile=true + continiousDetectLanguage=false.

--- start_stop.sh
+++ start_stop.sh
@@ -12,9 +12,15 @@ do
   INPUT="$TIMESTAMP$TIMESTAMP_HOUR"
   FILENAME="WHISPER_$INPUT"
   DESTINATION=""
+  mkdir "/Users/jibreelkeddo/DailyLogs/Whispers/"
+  mkdir "/Users/jibreelkeddo/DailyLogs/WhisperPages/"
   mkdir "/Users/jibreelkeddo/DailyLogs/Whispers/$TIMESTAMP_YEARMONTH/"
+  mkdir "/Users/jibreelkeddo/DailyLogs/WhisperPages/$TIMESTAMP_YEARMONTH/"
+  touch "/Users/jibreelkeddo/DailyLogs/Whispers/$TIMESTAMP_YEARMONTH/$FILENAME.txt"
+  touch "/Users/jibreelkeddo/DailyLogs/WhisperPages/$TIMESTAMP_YEARMONTH/$FILENAME.html"
 
-  ./stream -m models/ggml-large.bin -t 8 -tr -pc -f "/Users/jibreelkeddo/DailyLogs/Whispers/$TIMESTAMP_YEARMONTH/$FILENAME.html" --step 55000 --length 60000 --keep 5000 &  # Run the script in the background
+
+  ./stream -m models/ggml-large.bin -t 8 -tr -pc -f "/Users/jibreelkeddo/DailyLogs/Whispers/$TIMESTAMP_YEARMONTH/$FILENAME.txt" -fh "/Users/jibreelkeddo/DailyLogs/WhisperPages/$TIMESTAMP_YEARMONTH/$FILENAME.html" --step 57000 --length 57000 --keep 3000 &  # Run the script in the background
   sleep 1800  # Wait for 30 minutes
   echo "Killing the script..."
   pkill -f "./stream"  # Kill the process that matches the script name
