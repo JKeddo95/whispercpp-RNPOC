ac1d6af5fa958c5175f0a16377663e164628f511
jkeddo_ <jjkeddo@gmail.com>
2023-03-20 21:12:50 -0700

    POC-C--Revise-Keyword-Search

--- keywordSearch.sh
+++ keywordSearch.sh
@@ -1,41 +1,12 @@
-echo "Please enter the keyword you would like to search..."
-read SEARCHKEY
-
-
-echo "Searching for $SEARCHKEY"=
-
 FILENAME="/Users/jibreelkeddo/DailyLogs/Searches/SearchResults"
 TIMESTAMP_YEARMONTH=`date "+%Y%m"`
-echo "Searching for $SEARCHKEY"
+echo "Searching for $1"
 echo "Old search mods below"
 
-
 echo "<!DOCTYPE html><html><head><title>Whisper Search Results</title></head><body>" > $FILENAME.html
 echo "<link rel="stylesheet" href="./pico.css"></link><div &nbsp data-theme="dark">" >> $FILENAME.html
-grep -i -r -H "$SEARCHKEY" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/:/\<br>/;s/$/<br><br>/' >> $FILENAME.html
+grep -i -r -H "$1" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/:/\<br>/;s/$/<br><br>/' >> $FILENAME.html
 echo "</div></body></html>" >> $FILENAME.html
-
 
 open $FILENAME.html
-
-
-##################################################
-##### old/broken/retired keywordSearchQueries#####
-##################################################
-# grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' | less -R
-# grep -i -r "$1" /Users/jibreelkeddo/DailyLogs/Whispers/* | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' >> $FILENAME.html
-# grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/'
-##################################################
-##########Old/Broken Past Page Styling############
-##################################################
-echo "<!DOCTYPE html><html><head><title>Whisper Search Results</title></head><body>" > $FILENAME.html
-echo "<link rel="stylesheet" href="./pico.css"></link><div &nbsp data-theme="dark">" >> $FILENAME.html
-grep -i -r "$SEARCHKEY" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/$/<br><br>/' >> $FILENAME.html
-echo "</div></body></html>" >> $FILENAME.html
-##################################################
-##################################################
-
-
-echo "\n\nKeyword search completed!"
-
 exit
