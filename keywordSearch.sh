3f02313ca35b29bf89bc24ca4eeed408839fa27f
jkeddo_ <jjkeddo@gmail.com>
2023-03-20 21:09:03 -0700

    POC-C--Update-Keyword-Search

--- keywordSearch.sh
+++ keywordSearch.sh
@@ -2,17 +2,21 @@ echo "Please enter the keyword you would like to search..."
 read SEARCHKEY
 
 
+echo "Searching for $SEARCHKEY"=
+
 FILENAME="/Users/jibreelkeddo/DailyLogs/Searches/SearchResults"
 TIMESTAMP_YEARMONTH=`date "+%Y%m"`
 echo "Searching for $SEARCHKEY"
 echo "Old search mods below"
 
-echo "<html><body style=\"background-color:grey;\">" > $FILENAME.html
-grep -i -r "$1" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/$/<br><br>/' >> $FILENAME.html
-echo "</body></html>" >> $FILENAME.html
+
+echo "<!DOCTYPE html><html><head><title>Whisper Search Results</title></head><body>" > $FILENAME.html
+echo "<link rel="stylesheet" href="./pico.css"></link><div &nbsp data-theme="dark">" >> $FILENAME.html
+grep -i -r -H "$SEARCHKEY" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/:/\<br>/;s/$/<br><br>/' >> $FILENAME.html
+echo "</div></body></html>" >> $FILENAME.html
+
 
 open $FILENAME.html
-
 
 
 ##################################################
@@ -21,6 +25,13 @@ open $FILENAME.html
 # grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' | less -R
 # grep -i -r "$1" /Users/jibreelkeddo/DailyLogs/Whispers/* | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/' >> $FILENAME.html
 # grep -i -r --color=always "$SEARCHKEY" "/Users/jibreelkeddo/DailyLogs/Whispers" | sed 's/^\(.*\/\)\{0,1\}\([^\/]*\):\(.*\)$/\2:\3/'
+##################################################
+##########Old/Broken Past Page Styling############
+##################################################
+echo "<!DOCTYPE html><html><head><title>Whisper Search Results</title></head><body>" > $FILENAME.html
+echo "<link rel="stylesheet" href="./pico.css"></link><div &nbsp data-theme="dark">" >> $FILENAME.html
+grep -i -r "$SEARCHKEY" /Users/jibreelkeddo/DailyLogs/Whispers/* /Users/jibreelkeddo/DailyLogs/TODOs/* /Users/jibreelkeddo/DailyLogs/DailyLogs/*| sed 's/$/<br><br>/' >> $FILENAME.html
+echo "</div></body></html>" >> $FILENAME.html
 ##################################################
 ##################################################
 
