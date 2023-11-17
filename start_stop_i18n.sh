d5ea89685834cc0daa17cb51c052bb1f2b1f1d3a
jkeddo_ <jjkeddo@gmail.com>
2023-11-16 23:46:17 -0800

    POC-D--Upgrade-Startup-Scripts

--- start_stop_i18n.sh
+++ start_stop_i18n.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# bash ./models/download-ggml-model.sh large-v3
+
+## Backup for loop to keep trying to kill stream if it stays alive in BG
+pkill -f "./stream" && while pgrep -l "stream";
+  do sleep 1;
+done;
 
 while true
 do
   pkill -f "./stream"  # Kill the process that matches the script name
-  pkill -f "stream"  # Kill the process that matches the script name
   echo "Starting whisper cpp..."
 
   TIMESTAMP=`date "+%Y%m%d"`
@@ -22,13 +27,19 @@ do
 
 
 
-  ./stream -m models/ggml-large.bin -t 6 -pc --language "auto" -f "/Users/jibreelkeddo/DailyLogs/Whispers/$TIMESTAMP_YEARMONTH/$FILENAME.txt" -fh "/Users/jibreelkeddo/DailyLogs/WhisperPages/$TIMESTAMP_YEARMONTH/$FILENAME.html" --step 57000 --length 57000 --keep 3000 &  # Run the script in the background
+  ./stream -m models/ggml-large-v3.bin -t 6 -pc --language "auto" -f "/Users/jibreelkeddo/DailyLogs/Whispers/$TIMESTAMP_YEARMONTH/$FILENAME.txt" -fh "/Users/jibreelkeddo/DailyLogs/WhisperPages/$TIMESTAMP_YEARMONTH/$FILENAME.html" --step 57000 --length 57000 --keep 3000 &  # Run the script in the background
   sleep 1800  # Wait for 30 minutes
   echo "Killing the script..."
   pkill -f "./stream"  # Kill the process that matches the script name
-  pkill -f "stream"  # Kill the process that matches the script name
   wait
+  ## Backup for loop to keep trying to kill stream if it stays alive in BG
+  pkill -f "./stream" && while pgrep -l "stream";
+    do sleep 1;
+  done;
 done
 
 pkill -f "./stream"  # Kill the process that matches the script name
-pkill -f "stream"  # Kill the process that matches the script name
+## Backup for loop to keep trying to kill stream if it stays alive in BG
+pkill -f "./stream" && while pgrep -l "stream";
+  do sleep 1;
+done;
